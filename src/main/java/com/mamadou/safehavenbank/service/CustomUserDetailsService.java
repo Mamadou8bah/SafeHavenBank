e92ab8f871cc4a0731e1a9cc29ac4c7460e85459
mamadou8bah <mbah18791@gmail.com>
2025-10-05 17:35:02 +0000

    Implemented Custom Userdetails service for loading user by username

---
+++ src/main/java/com/mamadou/safehavenbank/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.mamadou.safehavenbank.service;
+
+import com.mamadou.safehavenbank.entity.User;
+import com.mamadou.safehavenbank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User>user =userRepository.findByEmail(email);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
